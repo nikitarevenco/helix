9f13ffd1036c302e1bdaf31dd4c8fcd1202ba981
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-18 15:05:00 +0000

    feat: remove tab keybindings

--- helix-term/src/ui/menu.rs
+++ helix-term/src/ui/menu.rs
@@ -274,12 +274,12 @@ impl<T: Item + 'static> Component for Menu<T> {
                 return EventResult::Consumed(close_fn);
             }
             // arrow up/ctrl-p/shift-tab prev completion choice (including updating the doc)
-            shift!(Tab) | key!(Up) | ctrl!('p') => {
+            key!(Up) | ctrl!('p') => {
                 self.move_up();
                 (self.callback_fn)(cx.editor, self.selection(), MenuEvent::Update);
                 return EventResult::Consumed(None);
             }
-            key!(Tab) | key!(Down) | ctrl!('n') => {
+            key!(Down) | ctrl!('n') => {
                 // arrow down/ctrl-n/tab advances completion choice (including updating the doc)
                 self.move_down();
                 (self.callback_fn)(cx.editor, self.selection(), MenuEvent::Update);
